d5b06806b6a06a0e4a0e25c780a201c0be10e3e8
Boubaker Joueid <joueid.boubaker0@gmail.com>
2023-06-06 14:22:37 +0200

    Update profil.view.php

--- views/Utilisateur/profil.view.php
+++ views/Utilisateur/profil.view.php
@@ -168,6 +168,7 @@ echo json_encode($events);
         </div>
       </div>
       <div class="col-lg-8">
+      <div class="col-lg-8">
   <div class="card mb-4">
     <div class="card-body">
       <div class="row">
@@ -179,38 +180,42 @@ echo json_encode($events);
         </div>
       </div>
       <hr>
-      <div class="col-sm-3">
+      <div class="row">
+        <div class="col-sm-3">
           <p class="mb-0">Email</p>
         </div>
-      <div id='mail'><?= $utilisateur['MAIL'] ?>
-    <button class="btn btn-transparent" id="btnModifMail"> 
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-</svg>
-    </button>
-</div>
-
-<div id="modificationMail" class= "d-none">
-    <form method="POST" action="<?= URL; ?>compte/validation_modificationMail">
-        <div class="row">
-            <label for="mail" class="col-2 col-form-label">Mail : </label>
-            <div class="col-8">
-                <input type="mail" class="form-control" name="MAIL" value="<?= $utilisateur['MAIL']?>"/>
-            </div>
-            <div class="col-2">
-                <button class="btn btn-success" id="btnValidModifMail">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-square" viewBox="0 0 16 16">
-                <path d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5H3z"/>
-                <path d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z"/>
-                </svg>
-                </button>
-            </div>
+        <div class="col-sm-6">
+          <p id='mail' class="text-muted mb-0"><?= $utilisateur['MAIL'] ?>
+          <button class="btn btn-transparent" id="btnModifMail"> 
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+          </svg>
+          </button>
+          </p>
         </div>
-    </form>
-</div>
-
+        <div id="modificationMail" class= "col-sm-3 d-none">
+          <form method="POST" action="<?= URL; ?>compte/validation_modificationMail">
+            <div class="row">
+              <label for="mail" class="col-2 col-form-label">Mail : </label>
+              <div class="col-8">
+                  <input type="mail" class="form-control" name="MAIL" value="<?= $utilisateur['MAIL']?>"/>
+              </div>
+              <div class="col-2">
+                  <button class="btn btn-success" id="btnValidModifMail">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-square" viewBox="0 0 16 16">
+                  <path d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5H3z"/>
+                  <path d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z"/>
+                  </svg>
+                  </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
       <hr>
+
+
       <div class="row">
         <div class="col-sm-3">
           <p class="mb-0">Téléphone</p>
