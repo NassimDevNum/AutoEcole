9d2e1545393e03349f2fc85978aece05b73c71ff
Boubaker Joueid <joueid.boubaker0@gmail.com>
2023-06-08 04:25:37 +0200

    modif

--- views/common/footer.php
+++ views/common/footer.php
@@ -1,39 +1,18 @@
-<div class="container-fluid">
+<div class="container-fluid white-text">
     <footer class="pt-4 my-md-5 pt-md-5 border-top">
         <div class="row">
         <div class="col-12 col-md">
-        <img src="<?= URL; ?>public/Assets/images/logo.png" width="40" alt="logo du site" />
-            <small class="d-block mb-3 text-muted">&copy; 2017–2021</small>
+        <img src="<?= URL; ?>public/Assets/images/logo.png" width="150" alt="logo du site" />
+            <small class="d-block mb-3 text-muted">&copy; tous droits réservés</small>
         </div>
         <div class="col-6 col-md">
-            <h5>Features</h5>
+            <h5>Contactez-nous</h5>
             <ul class="list-unstyled text-small">
-            <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Cool stuff</a></li>
-            <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Random feature</a></li>
-            <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Team feature</a></li>
-            <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Stuff for developers</a></li>
-            <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Another one</a></li>
-            <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Last time</a></li>
+            <li class="mb-1"><a class="link-secondary text-decoration-none" >123, rue de Lyon, 69000 Lyon</a></li>
+            <li class="mb-1"><a class="link-secondary text-decoration-none">01.45.55.15.41</a></li>
+            <li class="mb-1"><a class="link-secondary text-decoration-none">DriveMaster69@gmail.com</a></li>
+
             </ul>
-        </div>
-        <div class="col-6 col-md">
-            <h5>Resources</h5>
-            <ul class="list-unstyled text-small">
-            <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Resource</a></li>
-            <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Resource name</a></li>
-            <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Another resource</a></li>
-            <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Final resource</a></li>
-            </ul>
-        </div>
-        <div class="col-6 col-md">
-            <h5>About</h5>
-            <ul class="list-unstyled text-small">
-            <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Team</a></li>
-            <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Locations</a></li>
-            <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Privacy</a></li>
-            <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Terms</a></li>
-            </ul>
-        </div>
         </div>
     </footer>
 </div>
